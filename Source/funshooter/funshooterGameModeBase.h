4981befe3a77dc363ea8f6aa198bb71bfe4e9d3b
witoldm <witoldm@protonmail.ch>
2023-01-03 14:14:13 +0100

    Initial shooterproject

---
+++ Source/funshooter/funshooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "funshooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUNSHOOTER_API AfunshooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
